fe634617daf66a610f0fe142af12aded65ca32d9
Nikolay <nikolayshutikov@gmail.com>
2020-09-05 15:50:13 +0700

    single responsibility functions

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 let currentButton = "attack"; //two variants:skill or attack
 let skillButtonPressed = false;
 
-function pressAttackButton() {
-  currentButton = "attack";
-  // change view of attack button
-  let attackButton = document.querySelector(".attack-button");
-  attackButton.classList.add("attack-button_pressed");
-  attackButton.firstElementChild.classList.add(
-    "attack-button__hp-number_pressed"
-  );
-}
-
-function unpressAttackButton() {
-  let attackButton = document.querySelector(".attack-button");
-  attackButton.classList.remove("attack-button_pressed");
-  attackButton.firstElementChild.classList.remove(
-    "attack-button__hp-number_pressed"
-  );
-}
 function pressSkillButton() {
   skillButtonPressed = true;
   document
@@ -32,61 +15,95 @@ function unpressSkillButton() {
     .querySelector(".skills-button")
     .classList.remove("skills-button_activated");
 }
-function zombieAttacks() {
-  pressAttackButton();
-  disableSkillButton();
-  // disableAttackButton();
-  // play animation keyframes
-  let unitImage = document.querySelector(".unit_positioned_right")
-    .firstElementChild;
-  unitImage.classList.add("unit-image_attacks");
-  // play animation shleves in the same time
-  unitImage.firstElementChild.classList.add("unit-image__shleves_play");
+function transformZombieToRed() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.classList.add("unit-image_type_red-zombie");
+}
+function zombieSittingAnimationFinished() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.classList.remove("unit-image_sitting");
 }
 
-function zombieAnimationFinished() {
-  let unitImage = document.querySelector(".unit_positioned_right")
-    .firstElementChild;
-  if (currentButton === "skill") {
-    // unit-image_type_red-zombie
+function zombieAttackAnimationKeyframesFinished() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.classList.remove("unit-image_attacks");
+}
 
-    unitImage.classList.add("unit-image_type_red-zombie");
-    unitImage.classList.remove("unit-image_change-to-red-zombie");
-    // unitImage.firstElementChild.nextElementSibling.classList.remove(
-    //   "red-cloud_animation-played"
-    // );
+function zombieAttackAnimationShlevesFinished() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.firstElementChild.classList.remove(
+      "unit-image__shleves_play"
+    );
+}
+function zombieAnimationFinished() {
+  if (currentButton === "skill") {
+    transformZombieToRed();
+    zombieSittingAnimationFinished();
   } else if (currentButton === "attack") {
-    unitImage.classList.remove("unit-image_attacks");
-    unitImage.firstElementChild.classList.remove("unit-image__shleves_play");
+    zombieAttackAnimationKeyframesFinished();
+    zombieAttackAnimationShlevesFinished();
     enableSkillButton();
-    // enableAttackButton();
   }
 }
 
 function redCloudAnimationFinished() {
-  let unitImage = document.querySelector(".unit_positioned_right")
-    .firstElementChild;
-
-  unitImage.firstElementChild.nextElementSibling.classList.remove(
-    "red-cloud_animation-played"
-  );
+  let unitImage = document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.firstElementChild.nextElementSibling.classList.remove(
+      "red-cloud_animation-played"
+    );
   enableSkillButton();
   enableAttackButton();
+}
+
+function pressAttackButton() {
+  currentButton = "attack";
+
+  let attackButton = document.querySelector(".attack-button");
+  // change view of attack button
+  attackButton.classList.add("attack-button_pressed");
+  // change view of text inside button
+  attackButton.firstElementChild.classList.add(
+    "attack-button__hp-number_pressed"
+  );
 }
 
 function disableSkillButton() {
   document
     .querySelector(".skills-button")
-    .removeEventListener("mousedown", zombieSkillActivateListener);
+    .removeEventListener("mousedown", zombieSkillActivatedListener);
+}
+function playZombieAttackAnimationKeyFrames() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.classList.add("unit-image_attacks");
 }
 
-function disableAttackButton() {
+function playZombieAttackAnimationShleves() {
   document
-    .querySelector(".attack-button")
-    .removeEventListener("mousedown", zombieAttacks);
-  document
-    .querySelector(".attack-button")
-    .removeEventListener("mouseup", unpressAttackButton);
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.firstElementChild.classList.add(
+      "unit-image__shleves_play"
+    );
+}
+function zombieAttacks() {
+  pressAttackButton();
+  disableSkillButton();
+  playZombieAttackAnimationKeyFrames();
+  playZombieAttackAnimationShleves();
+}
+function unpressAttackButton() {
+  let attackButton = document.querySelector(".attack-button");
+  // change view of attack button
+  attackButton.classList.remove("attack-button_pressed");
+  // change view of text inside button
+  attackButton.firstElementChild.classList.remove(
+    "attack-button__hp-number_pressed"
+  );
 }
 function enableAttackButton() {
   document
@@ -97,62 +114,83 @@ function enableAttackButton() {
     .addEventListener("mouseup", unpressAttackButton);
 }
 
-function enableSkillButton() {
+function disableAttackButton() {
   document
-    .querySelector(".skills-button")
-    .addEventListener("mousedown", zombieSkillActivateListener);
-}
-function transformToRedZombie() {
-  disableSkillButton();
-  disableAttackButton();
-  // play animation
-  let unitImage = document.querySelector(".unit_positioned_right")
-    .firstElementChild;
-  unitImage.classList.add("unit-image_change-to-red-zombie");
-  // unitImage.classList.add("unit-image_type_red-zombie");
-  unitImage.firstElementChild.nextElementSibling.classList.add(
-    "red-cloud_animation-played"
-  );
+    .querySelector(".attack-button")
+    .removeEventListener("mousedown", zombieAttacks);
+  document
+    .querySelector(".attack-button")
+    .removeEventListener("mouseup", unpressAttackButton);
 }
 
+function unpressSkillButton() {
+  skillButtonPressed = false;
+  document
+    .querySelector(".skills-button")
+    .classList.remove("skills-button_activated");
+}
 function transformToDefaultZombie() {
   document
     .querySelector(".unit_positioned_right")
     .firstElementChild.classList.remove("unit-image_type_red-zombie");
 }
-function zombieSkillActivateListener() {
+
+function zombieSkillActivatedListener() {
   if (skillButtonPressed) {
     unpressSkillButton();
     transformToDefaultZombie();
-  }
-  // change view of skills button
-  else {
+  } else {
     pressSkillButton();
     transformToRedZombie();
   }
 }
+function enableSkillButton() {
+  document
+    .querySelector(".skills-button")
+    .addEventListener("mousedown", zombieSkillActivatedListener);
+}
+function playZombieAnimationSitting() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.classList.add("unit-image_sitting");
+}
+
+function playRedCloudAnimation() {
+  document
+    .querySelector(".unit_positioned_right")
+    .firstElementChild.firstElementChild.nextElementSibling.classList.add(
+      "red-cloud_animation-played"
+    );
+}
+function transformToRedZombie() {
+  disableSkillButton();
+  disableAttackButton();
+  playZombieAnimationSitting();
+  playRedCloudAnimation();
+}
+
 function addRedCloudAnimationListener() {
   document
     .querySelector(".red-cloud")
     .addEventListener("animationend", redCloudAnimationFinished);
 }
-function main() {
-  console.log("in_main");
-  // zombieAttack animation finished listener
 
+function addRedZombieAnimationListener() {
   document
     .querySelector(".unit_positioned_right")
     .firstElementChild.addEventListener(
       "animationend",
       zombieAnimationFinished
     );
+}
+
+function main() {
+  addRedZombieAnimationListener();
+
   addRedCloudAnimationListener();
-  // zombieSkillActivateListenerd animation finished listener
-  // document.querySelector()
-  // button attack
+
   enableAttackButton();
 
-  // button skills
   enableSkillButton();
 }
 
